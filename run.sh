a1094a207ea55c63160b5d069ab0ac931e4cd6a5
Виктор Мельников <v.melnikov@dengionline.com>
2015-03-24 11:54:25 +0300

    Update run

--- run.sh
+++ run.sh
@@ -1,11 +1,12 @@
 #!/bin/bash
 BASE_PATH=$(pwd)
 CONTAINER="web-server"
-sed "/#${CONTAINER}/d" /etc/hosts > /etc/hosts
+if [ ! -e /etc/hosts-backup ]; then
+    cp /etc/hosts /etc/hosts-backup
+fi
+sed -i "/#${CONTAINER}/d" /etc/hosts
 CID=$(docker run -d -p 80 -p 443 -p 9009 -v ${BASE_PATH}/nginx-log:/var/log/nginx -v ${BASE_PATH}/var-www:/var/www -v ${BASE_PATH}/vhosts:/etc/nginx/vhosts ${CONTAINER})
 echo "Container ID -${CID}"
 IP=$(docker inspect --format='{{.NetworkSettings.IPAddress}}' ${CID})
 echo "IP - ${IP}"
-awk=$(echo "\$1 ~ /server_name/{print \"${IP}\t\", substr(\$2,1,length(\$2)-1), \"\t#${CONTAINER}\"}") 
-echo ${awk}
-awk ${AWK} ${BASE_PATH}/vhosts/*.conf 
+awk -v ip=${IP} -v container=${CONTAINER} '$1 ~ /server_name/{print ip gensub(/server_name\W+([^;]+);/,"\\1",$0),"#" container}' ${BASE_PATH}/vhosts/*.conf >> /etc/hosts
