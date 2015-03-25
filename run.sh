6f5cdf82dcdf81a15c5deac612e5574703913544
Viktor Melnikov <chipx@mail.ru>
2015-03-26 01:03:34 +0300

    Update runner

--- run.sh
+++ run.sh
@@ -5,7 +5,7 @@ if [ ! -e /etc/hosts-backup ]; then
     cp /etc/hosts /etc/hosts-backup
 fi
 sed -i "/#${CONTAINER}/d" /etc/hosts
-CID=$(docker run -d -p 80 -p 443 -p 9009 -v ${BASE_PATH}/nginx-log:/var/log/nginx -v ${BASE_PATH}/var-www:/var/www -v ${BASE_PATH}/vhosts:/etc/nginx/vhosts ${CONTAINER})
+CID=$(docker run -d --link redis:redis.local -p 80 -p 443 -p 9009 -v ${BASE_PATH}/nginx-log:/var/log/nginx -v ${BASE_PATH}/var-www:/var/www -v ${BASE_PATH}/vhosts:/etc/nginx/vhosts ${CONTAINER})
 echo "Container ID -${CID}"
 IP=$(docker inspect --format='{{.NetworkSettings.IPAddress}}' ${CID})
 echo "IP - ${IP}"
